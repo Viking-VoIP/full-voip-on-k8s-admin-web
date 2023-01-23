fb0c2d90d3167d4692abb051ce6f766833055a6b
David Villasmil <david.villasmil.work@gmail.com>
2023-01-23 20:58:52 +0100

    Fix db_host

--- start.sh
+++ start.sh
@@ -3,10 +3,9 @@
 #bash /start_consul_agent.sh &
 #sleep 3
 
-DB_ADDRESS=$(consul kv get backend/db_address)
+DB_HOST=$(consul kv get backend/db_address)
 DB_USER=$(consul kv get backend/db_user)
 DB_PASS=$(consul kv get backend/db_pass)
-DB_HOST=$(consul kv get backend/db_host)
 
 sed -i "s/{{ DB_USER }}/$DB_USER/g" /var/www/html/conexion.inc
 sed -i "s/{{ DB_PASS }}/$DB_PASS/g" /var/www/html/conexion.inc
